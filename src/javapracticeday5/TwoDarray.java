bfe463af48bfc601cff4b88229912a7d5230838b
RAHUL LABADE <rahullabade05@gmail.com>
2021-09-30 19:14:45 +0530

    ADDED: TwoDarray.java

---
+++ src/javapracticeday5/TwoDarray.java
@@ -0,0 +1,34 @@
+package javapracticeday5;
+
+import java.util.Scanner;
+
+public class TwoDarray {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number for rows and column : ");
+		int row = scanner.nextInt();
+		int column = scanner.nextInt();
+		int array[][] = new int[row][column];
+		int i,j;
+		for(i=0; i<row; i++)
+		{
+			for(j=0; j<column; j++)
+			{
+				System.out.println("Enter the element of Array");
+				int element = scanner.nextInt();
+				array[i][j] = element;
+			}
+		}
+		for(i=0; i<row; i++)
+		{
+			System.out.println();
+			for(j=0; j<column; j++)
+		
+			{
+				System.out.println(" " +array[i][j]);
+			}
+		}
+		
+	}
+}
