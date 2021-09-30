51425fea7d5b6cffe42b84f08b5338d63c0a62ba
RAHUL LABADE <rahullabade05@gmail.com>
2021-09-30 09:38:19 +0530

    refactored: FlipCoin.java

--- src/javapracticeday5/FlipCoin.java
+++ src/javapracticeday5/FlipCoin.java
@@ -1,37 +1,24 @@
 package javapracticeday5;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class FlipCoin {
 
 	public static void main(String[] args) {
+		int n;
 		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		int x = 0,y = 0;
-		for (int i = 0; i <= number; i++) {
-			Random r = new Random();
-			int n = r.nextInt(2);
-			if (n < 1) {
-				x++;
-			}
-			else {
-				y++;
-			}
-		}
-			double p;
-			double total = x + y;
-			if (x > y) {
-				p = (float)((x / total)*100);
-				System.out.println("Heads win");
-				System.out.println("Heads win by" + p + "percentage");
-				}
-			else
-			{
-				p = (float)((y / total)*100);
-				System.out.println("Tail win");
-				System.out.println("Tail win by" + p + "percentage");
-			}
+		System.out.println("Enter number of times you want to flip:");
+		n = sc.nextInt();
+		getValues(n);
 	}
-
+	public static void getValues(int n) {
+		for(int i=0; i <n; i++)
+		{
+			double value = Math.floor(Math.random()*10) % 2;
+			if (value < 0.5)
+				System.out.println("Tails");
+			else
+				System.out.println("Heads");
+		}
+	}
 }
